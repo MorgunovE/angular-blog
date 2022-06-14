382280be60dacae253aef7a384985abfc37cc819
MorgunovE <morgunove87@gmail.com>
2022-06-14 16:00:11 +0400

    guard2

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {User} from "../../shared/interfaces";
 import {AuthService} from "../shared/services/auth.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -13,12 +13,21 @@ export class LoginPageComponent implements OnInit {
   // @ts-ignore
   form: FormGroup
   submitted = false
+  message: string
+
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params:Params)=>{
+      if(params['loginAgain']){
+        this.message = 'Please Sing In'
+      }
+    })
+
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
