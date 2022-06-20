d265de8bdf642ea5de4ab4a16f8ffc05e94f99b8
MorgunovE <morgunove87@gmail.com>
2022-06-20 16:54:36 +0400

    create dashboard

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../../shared/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = []
+  pSub: Subscription
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+  ngOnDestroy() {
+    if(this.pSub){
+      this.pSub.unsubscribe()
+    }
   }
 
+  remove(id: string) {
+
+  }
 }
--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -20,4 +20,16 @@ export class PostsService {
         }
       }))
   }
+  getAll(): Observable<Post[]>{
+    return this.http.get(`${environment.fbDbUrl}/posts.json`)
+      .pipe(map((response: {[key: string]: any})=>{
+        return Object
+          .keys(response)
+          .map(key => ({
+            ...response[key],
+            id: key,
+            date: new Date(response[key].date)
+          }))
+      }))
+  }
 }
